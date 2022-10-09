62b25493505ff3fa642d54ce73dcf7287a5544c6
Robin Koechel <robin_koechel@protonmail.com>
2022-10-09 03:19:02 +0200

    some Parser stuff

--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
 public class Parser{
     public Parser(){
-        
+
     }
+    //teilt den eingegebenen String in einzelne Fragmente auf 
+    //und gibt sie dann alle als ein Objekt des Typs Funktion zurück                                   
+    public Funktion stringZuFunktion(String input){
+        String in = transformString(input);
+        ArrayList<String> monome = new ArrayList<String>();
+
+        
+
+
+        return null;
+    }
+    private Funktion latexZuFunktion(){ //wandelt eins der String Fragmente in Funktion
+
+        return null;
+    }
+    //checks if operation is in funktion string
+    public boolean isOperationInFunctionString(String strOfFunktion, String strOfOperation){
+        if(strOfFunktion.contains(strOfOperation))return true;
+        else return false;
+    }
+
+    public String transformString(String in){ //clean the input string
+        String output = in;
+        output = output.strip();
+        return output;
+    }
+
 }
