be5db650f434a3171f3082cfbbe9f324d503f99b
hah23 <73662688+hah23@users.noreply.github.com>
2022-10-09 07:09:30 +0100

    Add files via upload

--- src/Cosinus.java
+++ src/Cosinus.java
@@ -1,4 +1,19 @@
 
-public class Cosinus {
+public class Cosinus extends TrigonometricFunctions {
+
+	public Cosinus(double c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public Function derivative() {
+		Function f = new Sinus(getCoefficient() *-1);
+		ConnectTwoFunctions c= new ConnectTwoFunctions(f, getX().derivative(), '*');
+		return f;
+	}
+	@Override
+	public double getY(double x) {
+		return getCoefficient()* Math.cos(getX().getY(x));
+	}
 
 }
